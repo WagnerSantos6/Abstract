88fb75506fc52a3ffd4ef4a5f295a02811bccfea
Wagner Santos <wagnerstss@gmail.com>
2022-09-26 17:34:25 -0300

    Abstrato

---
+++ Abstract/Entities/TaxPayer.cs
@@ -0,0 +1,17 @@
+﻿using Abstract.Entities;
+
+namespace Abstract.Entities
+{
+    internal abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+    public abstract double Tax();
+    }
+}
